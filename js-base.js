d8759fa883ec3ccaef5df8aadfd5b70cefbcd833
chenjirui <1391621151@qq.com>
2017-08-04 13:31:22 +0800

    add js center

--- js-base.js
+++ js-base.js
@@ -20,6 +20,19 @@ function ulControl() {
     $("sign").css("display", "block");
 
 }
+
+function setCenter() {
+    if (window.screen.width > window.screen.height) {
+        $("body").width("50%");
+        $("body").css({
+            position: "absolute",
+            left: $(window).width()/4
+        });
+    }
+}
+
+
 $(document).ready(function () {
     ulControl();
+    setCenter();
 });
